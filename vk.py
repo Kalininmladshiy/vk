75b6787979dc31d44c0701a5e316bd21225dc7b8
Максим Калинин <maksim.kalinin1@gmail.com>
2022-09-10 15:15:10 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -6,7 +6,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-def get_comics(url):
+def get_comics(comics_num=None):
+    if not comics_num:
+        url = f'https://xkcd.com/info.0.json'
+    else:
+        url = f'https://xkcd.com/{comics_num}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
@@ -22,10 +26,18 @@ def send_picture(url, path_to_pictures, filename, key):
         return response.json()
 
 
-def dowload_picture_to_vk(url, payload):
+def dowload_picture_to_vk(method_name, payload):
+    url = f'https://api.vk.com/method/{method_name}'
     response = requests.post(url, params=payload)
     response.raise_for_status()
     return response.json()
+
+
+def make_get_request_to_vk(method_name, payload):
+    url = f'https://api.vk.com/method/{method_name}'
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()    
 
 
 if __name__ == '__main__':
@@ -33,14 +45,9 @@ if __name__ == '__main__':
     client_id = os.environ['VK_CLIENT_ID']
     group_id = os.environ['VK_GROUP_ID']
     access_token = os.environ['VK_ACCESS_TOKEN']
-    last_comics_url = 'https://xkcd.com/info.0.json'
-    get_wall_server_url = 'https://api.vk.com/method/photos.getWallUploadServer'
-    save_wall_photo_url = 'https://api.vk.com/method/photos.saveWallPhoto'
-    wall_post_url = 'https://api.vk.com/method/wall.post'
-    last_comics = get_comics(last_comics_url)
+    last_comics = get_comics()
     comics_num = random.randint(1, last_comics['num'])
-    random_comics_url = f'https://xkcd.com/{comics_num}/info.0.json'
-    random_comics = get_comics(random_comics_url)
+    random_comics = get_comics(comics_num)
     download_pictures(Path.cwd(), 'python.png', random_comics['img'])
     comment = random_comics['alt']
     payload = {
@@ -48,9 +55,8 @@ if __name__ == '__main__':
         'access_token': access_token,
         'v': '5.131',
      }
-    response = requests.get(get_wall_server_url, params=payload)
-    response.raise_for_status()
-    upload_url = response.json()['response']['upload_url']
+    wall_server = make_get_request_to_vk('photos.getWallUploadServer', payload)
+    upload_url = wall_server['response']['upload_url']
     params_for_save_image = send_picture(upload_url, Path.cwd(), 'python.png', 'photo')
     payload = {
         'group_id': group_id,
@@ -60,7 +66,7 @@ if __name__ == '__main__':
         'server': params_for_save_image['server'],
         'hash': params_for_save_image['hash'],
      }
-    params_for_post_photo = dowload_picture_to_vk(save_wall_photo_url, payload)
+    params_for_post_photo = dowload_picture_to_vk('photos.saveWallPhoto', payload)
     owner_id = params_for_post_photo['response'][0]['owner_id']
     media_id = params_for_post_photo['response'][0]['id']
     payload = {
@@ -71,6 +77,6 @@ if __name__ == '__main__':
         'message': comment,
         'attachments': f"photo{owner_id}_{media_id}",
      }
-    dowload_picture_to_vk(wall_post_url, payload)
+    dowload_picture_to_vk('wall.post', payload)
     os.remove(Path.cwd() / 'python.png')
     
