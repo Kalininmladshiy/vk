2544c391fe95c5cbeb821da1045a05b0f1f9b14d
Максим Калинин <maksim.kalinin1@gmail.com>
2022-09-10 15:30:02 +0300

    Removed unused function

--- utils.py
+++ utils.py
@@ -13,8 +13,3 @@ def download_pictures(
     response.raise_for_status()
     with open(Path() / path_to_pictures / filename, 'wb') as file:
         file.write(response.content)
-
-def get_file_extension(url):
-    url_parts = urlparse(url)
-    path, file_extension = os.path.splitext(url_parts.path)
-    return file_extension
